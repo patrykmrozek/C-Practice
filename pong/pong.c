d5cea7df9ca72716c626b13382fd921fb4dee9f8
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-03-14 12:46:56 +0000

    feat: added dynamic scoreboard, imporved ball physics

--- pong/pong.c
+++ pong/pong.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <math.h>
+#include <SDL2/SDL_ttf.h>
 
 #define WIDTH 1000
 #define HEIGHT 700
@@ -14,9 +15,12 @@
 #define PLAYER_2_Y HEIGHT/2
 #define SPEED 10
 #define BALL_SPEED 10
+#define BALL_SIZE 15
+
 
 typedef struct {
 	double x, y, w, h;
+	int score;
 } Player;
 
 
@@ -51,7 +55,6 @@ void in_bounds(Player* players[]) {
 }
 
 void draw_circle(SDL_Surface* surface, Circle circle, Uint32 color) {
-
 	for (double x=circle.x-circle.r; x<circle.x+circle.r; x++) {
 		for (double y=circle.y-circle.r; y<circle.y+circle.r; y++) {
 			double distance = sqrt(pow((circle.x-x), 2)+pow((circle.y-y), 2));
@@ -75,25 +78,22 @@ void reset(Circle* ball, Player* p1, Player* p2) {
 	// rand()%2 generates a random num between 0 and 1
 	// randomly assigns the balls' v_x and v_y
 	ball->v_x = (rand()%2==0 ? BALL_SPEED: -BALL_SPEED);
-	ball->v_y = (rand()%2==0 ? BALL_SPEED : -BALL_SPEED);
+	ball->v_y = (rand()%2==0 ? BALL_SPEED/2 : -BALL_SPEED/2);
 	SDL_Delay(1000);
+	printf("PLAYER 1: %d\nPLAYER 2: %d\n\n", p1->score, p2->score);
 }
 
-void step(Circle* circle, Player* players[], int* score1, int* score2){
+void step(Circle* circle, Player* players[]){
 	if (circle->x > WIDTH) {
 		reset(circle, players[0], players[1]);
-		(*score2)++;
+		players[0]->score++;
 	} else if (circle->x < 0) {
 		reset(circle, players[0], players[1]);
-		(*score1)++;
+		players[1]->score++;
 	}
 	for (int i=0; i<2; i++){
 		if (check_collision(circle, players[i])) {
 			circle->v_x = -circle->v_x;
-			//adjusts angle of the ball based on where it was hit
-			double offset = (circle->y - ((players[i]->y + players[i]->h/2)
-					/players[i]->h/2));
-			circle->v_y = offset*BALL_SPEED;
 		}
 	}
 	if (circle->y <= 0 || circle->y >= HEIGHT - circle->r) {
@@ -114,6 +114,7 @@ void input_handle(Player* player1, Player* player2) {
 int main() {
 
 	SDL_Init(SDL_INIT_VIDEO);
+	TTF_Init();
 	SDL_Window* window = SDL_CreateWindow(
 		"Pong",
 		SDL_WINDOWPOS_CENTERED,
@@ -124,26 +125,31 @@ int main() {
 
 	SDL_Surface* surface = SDL_GetWindowSurface(window);
 	
-	SDL_RaiseWindow(window);
+	TTF_Font* font = TTF_OpenFont("Roboto-Regular.ttf", 24);
+	if (!font) {
+		printf("font not loaded\n");
+	}
+		SDL_RaiseWindow(window);
 	SDL_PumpEvents();
 	Player player1 = {
 		PLAYER_1_X-(PLAYER_W/2),
 		PLAYER_1_Y-(PLAYER_H/2),
 		PLAYER_W,
-		PLAYER_H
+		PLAYER_H,
+		0
 	};
 	Player player2 = {
 		PLAYER_2_X-(PLAYER_W/2),
 		PLAYER_2_Y-(PLAYER_H/2),
 		PLAYER_W,
-		PLAYER_H
+		PLAYER_H,
+		0
 	};
-	
+	SDL_Color text_color = {255, 255, 255, 255};
+
 	Player* players[] = {&player1, &player2};
 	
-	int score1=0, score2=0;
-
-	Circle circle = {WIDTH/2, HEIGHT/2, 15, BALL_SPEED, 2};
+	Circle circle = {WIDTH/2, HEIGHT/2, BALL_SIZE, BALL_SPEED, BALL_SPEED};
 	reset(&circle, &player1, &player2);
 	SDL_Rect erase = (SDL_Rect){0, 0, WIDTH, HEIGHT};
 	SDL_Event event;
@@ -154,17 +160,34 @@ int main() {
 				program_running = 0;
 			}
 		}
+	
 
 		input_handle(&player1, &player2);
 		in_bounds(players);
-			
+		
+		char score_text[20];
+		// snprintf -> formats a string and stores it in a character array
+		// syntax: snprintf(char *str, size_t size, const char *format, ...);
+		snprintf(score_text, sizeof(score_text), "%d : %d", player1.score, player2.score);
+		SDL_Surface* text_surface = TTF_RenderText_Solid(font, score_text, text_color);
+		SDL_Rect text_rect = (SDL_Rect){WIDTH/2-50, 20, 100, 40};	
+
+
+	
 		SDL_FillRect(surface, &erase, COLOR_BLACK);
+		SDL_BlitSurface(text_surface, NULL, surface, &text_rect);
 		draw_player(surface, &player1);
 		draw_player(surface, &player2);
 		draw_circle(surface, circle, COLOR_WHITE);
-		step(&circle, players, &score1, &score2);
+		step(&circle, players);
 		SDL_UpdateWindowSurface(window);
 		SDL_Delay(16); //approx 60 fps
-		printf("SCORE1: %d\nSCORE2: %d\n", score1, score2);
+		
+		SDL_FreeSurface(text_surface);
 	}
+	SDL_Quit();
+	SDL_DestroyWindow(window);
+	return 0;
+	
 }
+
